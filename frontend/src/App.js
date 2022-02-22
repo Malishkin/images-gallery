14dafa75ec793639f5baf54ae928c2831c41e273
Malishkin <malishkin2002@gmail.com>
2022-02-23 00:05:16 +0200

    save images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,28 +3,35 @@ import Header from './components/Header';
 import Search from './components/Search';
 import { useState } from 'react';
 
-const UNSPLASH_ACCESS_KEY=process.env.REACT_APP_UNSPLASH_ACCESS_KEY
+const UNSPLASH_ACCESS_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+
 const App = () => {
-  const [word, setWord]=useState('');
-  const handleSearchSubmit =(e)=>{
+  const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+  console.log(images);
+  const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_ACCESS_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_ACCESS_KEY}`
+    )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
+        setImages([data, ...images]);
+        
       })
       .catch((err) => {
         console.log(err);
-      })
-      setWord('');
-  }
+      });
+    setWord('');
+  };
   // console.log(process.env);
   // console.log("UNSPLASH_ACCESS_KEY: ", UNSPLASH_ACCESS_KEY);
   return (
     <div>
-      <Header title ="Images Gallery"/>
-      <Search handleSubmit={handleSearchSubmit} word={word} setWord={setWord}/>
+      <Header title='Images Gallery' />
+      <Search handleSubmit={handleSearchSubmit} word={word} setWord={setWord} />
     </div>
   );
 };
