72b9cfb8b875c253ce9bb7f6777a56828b8963b3
Malishkin <malishkin2002@gmail.com>
2022-02-23 10:46:20 +0200

    display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
 import { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_ACCESS_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 
 const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
-  console.log(images);
+
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(word);
+
     fetch(
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_ACCESS_KEY}`
     )
       .then((res) => res.json())
       .then((data) => {
         // console.log(data);
-        setImages([data, ...images]);
-        
+        setImages([{ ...data, title: word }, ...images]);
       })
       .catch((err) => {
         console.log(err);
@@ -28,10 +29,23 @@ const App = () => {
   };
   // console.log(process.env);
   // console.log("UNSPLASH_ACCESS_KEY: ", UNSPLASH_ACCESS_KEY);
+
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id));
+  };
   return (
     <div>
       <Header title='Images Gallery' />
       <Search handleSubmit={handleSearchSubmit} word={word} setWord={setWord} />
+      <Container className='mt-4'>
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, index) => (
+            <Col key={index} className='pb-3'>
+              <ImageCard image={image} deleteImage={handleDeleteImage} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
