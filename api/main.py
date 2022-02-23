50fad66d4ca0cde61a07cadf98175ac28720b96e
Malishkin <malishkin2002@gmail.com>
2022-02-23 21:14:19 +0200

    sample flask app

--- api/main.py
+++ api/main.py
@@ -1,9 +1,21 @@
 # save this as app.py
+
 from flask import Flask
 
-app = Flask(__name__)
+
+app = Flask(__name__)  # main 
+
+
+# def hello():
+#     return "Hello, World!"
+# app.route("/")(hello)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
-app.run(port=5000) 
+
+if __name__ =="__main__":
+    app.run(host="0.0.0.0", port=5050)
+
+
+# app.run(port=5000)
